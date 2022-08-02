23feb2d0ccff61838c463b9d88a9a3d27175162d
JungHwan <74060017+wjdghks963@users.noreply.github.com>
2022-08-02 22:23:13 +0900

    Update 01_find_max_number.kt
    주석 추가

--- app/src/main/java/com/example/algorithm_kotlin/week_1/01_find_max_number.kt
+++ app/src/main/java/com/example/algorithm_kotlin/week_1/01_find_max_number.kt
@@ -3,11 +3,14 @@ package com.example.algorithm_kotlin.week_1
 import android.app.Activity
 
 class find_max_number: Activity() {
+    // 배열 선언
     val list = listOf<Int>(3,5,6,1,2,4)
-
+    // 함수 선언 return 값은 Int형
   fun find_max_num(list: List<Int>): Int{
       for( num in list){
+          // list안의 하나의 value1를 잡아서
           for( num2 in list){
+              // list안의 하나의 value1과 대조할 것을 잡아 if문 안에서  더 큰 값이 있다면 잡는다
               if(num < num2){
                   break
               }else{
@@ -17,7 +20,6 @@ class find_max_number: Activity() {
       }
       return 1
   }
-
     val result = find_max_num(list)
 
 }
