9449c403f6b7d33eb8496ff54ab68f9e6bcf0f04
wjdghks963 <chsw000@naver.com>
2022-08-02 22:32:01 +0900

    day 2

--- app/src/main/java/com/example/algorithm_kotlin/week_1/01_find_max_number.kt
+++ app/src/main/java/com/example/algorithm_kotlin/week_1/01_find_max_number.kt
@@ -2,24 +2,26 @@ package com.example.algorithm_kotlin.week_1
 
 import android.app.Activity
 
-class find_max_number: Activity() {
+class FindMaxNumber01 : Activity() {
     // 배열 선언
-    val list = listOf<Int>(3,5,6,1,2,4)
+    val list = listOf<Int>(3, 5, 6, 1, 2, 4)
+
     // 함수 선언 return 값은 Int형
-  fun find_max_num(list: List<Int>): Int{
-      for( num in list){
-          // list안의 하나의 value1를 잡아서
-          for( num2 in list){
-              // list안의 하나의 value1과 대조할 것을 잡아 if문 안에서  더 큰 값이 있다면 잡는다
-              if(num < num2){
-                  break
-              }else{
-                  return num
-              }
-          }
-      }
-      return 1
-  }
+    fun find_max_num(list: List<Int>): Int {
+        for (num in list) {
+            // list안의 하나의 value1를 잡아서
+            for (num2 in list) {
+                // list안의 하나의 value1과 대조할 것을 잡아 if문 안에서  더 큰 값이 있다면 잡는다
+                if (num < num2) {
+                    break
+                } else {
+                    return num
+                }
+            }
+        }
+        return 1
+    }
+
     val result = find_max_num(list)
 
 }
---
+++ app/src/main/java/com/example/algorithm_kotlin/week_1/02_find_max_num.kt
@@ -0,0 +1,18 @@
+package com.example.algorithm_kotlin.week_1
+
+class FindMaxNumber02 {
+    val list = listOf(3,5,6,1,2,4)
+
+    fun findMaxNum(list:List<Int>): Int {
+        // 우선 제일 큰 수를 뜻하는 변수를 선언한다
+        var maxNum = list[0]
+        // 반복문을 통해 list 안에서 변수에 저장되는 값보다 큰값이 있다면 변수에 저장한다.
+        for (num in list){
+            if(num > maxNum){
+                maxNum = num
+            }
+        }
+        return maxNum
+    }
+
+}
