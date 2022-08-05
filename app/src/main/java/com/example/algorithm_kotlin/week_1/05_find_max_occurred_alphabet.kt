86c5d172450c5dea6c854741d8cd37577d1a3c76
JungHwan <74060017+wjdghks963@users.noreply.github.com>
2022-08-05 17:40:47 +0900

    day 5

--- app/src/main/java/com/example/algorithm_kotlin/week_1/05_find_max_occurred_alphabet.kt
+++ app/src/main/java/com/example/algorithm_kotlin/week_1/05_find_max_occurred_alphabet.kt
@@ -7,9 +7,9 @@ class FindMaxOccurredAlphabet {
         // 0으로 채워진 26개의 수정 가능한 배열을 생성
         val list = MutableList(26) { 0 }
         
-        // 최고 빈도수 최고 알파벳
-        var maxOccurrence = 0
-        var maxAlphabet = list[0]
+        // 최고 빈도수 최고 알파벳 index
+        var maxOcc = 0
+        var maxAlIndex = 0
 
         // list에 있는 al을 비교해서 해당하는 알파벳 갯수를 알파벳 순서에 맞게 넣는다
         for (al in string) {
@@ -21,8 +21,14 @@ class FindMaxOccurredAlphabet {
            }
         }
         
-        for()
-
+        for(index in list.size){
+            var alOcc = list[index]
+            if(alOcc > maxOcc){
+                maxAlIndex = index
+                maxOcc = alOcc
+            }
+        }
+        return (maxAlIndex + 'a'.toInt()).toChar
     }
 
 
