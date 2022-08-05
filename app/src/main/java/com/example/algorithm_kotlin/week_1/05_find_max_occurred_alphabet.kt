9d43aea599262f121406ab21e1feadcc16016e64
JungHwan <74060017+wjdghks963@users.noreply.github.com>
2022-08-05 17:17:18 +0900

    Create 05_find_max_occurred_alphabet.kt

---
+++ app/src/main/java/com/example/algorithm_kotlin/week_1/05_find_max_occurred_alphabet.kt
@@ -0,0 +1,35 @@
+package com.example.algorithm_kotlin.week_1
+
+import android.util.Log
+
+class FindMaxOccurredAlphabet {
+    fun find_max_occurred_alphabet(string: String): MutableList<Int>  {
+        // 0으로 채워진 26개의 수정 가능한 배열을 생성
+        val list = MutableList(26) { 0 }
+        
+        // 최고 빈도수 최고 알파벳
+        var maxOccurrence = 0
+        var maxAlphabet = list[0]
+
+        // list에 있는 al을 비교해서 해당하는 알파벳 갯수를 알파벳 순서에 맞게 넣는다
+        for (al in string) {
+           if (index.toString() in 'A'..'Z' && index.toString() in 'a'..'z'){
+             var index = al.toInt() - 'a'.toInt()
+             list[index] += 1
+           }else{
+             continue
+           }
+        }
+        
+        for()
+
+    }
+
+
+    // result -> l
+    fun result() {
+        Log.d("result",find_max_occurred_alphabet("hello my name is chris"))
+    }
+
+}
+Footer
